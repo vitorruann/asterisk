902413b292e93efb63738647890bd95cde6e92ba
Vitor Ruan <vitorruann@gmail.com>
2021-07-23 10:03:32 -0300

    Retirado rota teste

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -16,13 +16,6 @@ routes.get('/sipFunctions', sipCallController.callFunctions);
 routes.get('/call/:typeCall/', sipCallController.callStatus);
 routes.get('/:statusPhone', sipCallController.infPhone);
 
-routes.get("/cgi-bin/ConfigManApp.com", (req, res) => {
-    console.log("Big acessou!", req)
-    const key = req.query.key
-    const code = req.query.code
-    return res.json({ key, code});
-  });
-
 export default routes;
 
 // http://10.1.31.92:3334/registered
